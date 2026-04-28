2d6485c72c3ebb68cb3658f66483414f8e220d76
pagliares <pagliares@gmail.com>
2026-04-28 11:44:17 -0300

    refactor: apply extract class refactor to Guest class

--- Source_Code/hostelapp/src/main/java/hostealapp/model/Guest.java
+++ Source_Code/hostelapp/src/main/java/hostealapp/model/Guest.java
@@ -4,17 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //@Getter
 //@Setter
 //@AllArgsConstructor
 public class Guest {
     private String name;
     private String lastName;
-    private String address;
-    private String zipCode;
-    private String city;
-    private String state;
+
     private String email;
+
+    private Address address;
 
 
     public Guest() {
@@ -46,36 +48,12 @@ public class Guest {
         this.lastName = lastName;
     }
 
-    public String getAddress() {
+    public Address getAddress() {
         return address;
     }
 
-    public void setAddress(String address) {
+    public void setAddress(Address address) {
         this.address = address;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
     }
 
     public String getEmail() {
