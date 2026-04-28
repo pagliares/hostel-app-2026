0f201e7270716fee137b722c53da9b90ac1af3f8
pagliares <pagliares@gmail.com>
2026-04-28 11:10:22 -0300

    feat: Add address information to Guest class

--- Source_Code/hostelapp/src/main/java/hostealapp/model/Guest.java
+++ Source_Code/hostelapp/src/main/java/hostealapp/model/Guest.java
@@ -10,6 +10,12 @@ import lombok.Setter;
 public class Guest {
     private String name;
     private String lastName;
+    private String address;
+    private String zipCode;
+    private String city;
+    private String state;
+    private String email;
+
 
     public Guest() {
         this(null);
@@ -38,6 +44,46 @@ public class Guest {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public static void main(String[] args) {
--- Source_Code/hostelapp/src/main/java/hostealapp/model/GuestTest.java
+++ Source_Code/hostelapp/src/main/java/hostealapp/model/GuestTest.java
@@ -6,14 +6,37 @@ public class GuestTest {
         Guest guest = new Guest();
         guest.setName("Atticus");
         guest.setLastName("Finch");
+        guest.setCity("Alfenas");
+        guest.setState("MG");
+        guest.setAddress("Rua Governador Pablo Neruda");
+        guest.setZipCode("37500-000");
+        guest.setEmail("finch@gmail.com");
 
-        System.out.println(guest.getName() + " " + guest.getLastName());
+        GuestTest x = new GuestTest();
+        x.exibirDados(guest);
 
-        Guest guest2 = new Guest();
-        guest2.setName("Fermina");
-        guest2.setLastName("Daza");
+        guest = new Guest();
+        guest.setName("Fermina");
+        guest.setLastName("Daza");
+        guest.setCity("Las Vegas");
+        guest.setState("Nevada");
+        guest.setAddress("Main Street");
+        guest.setZipCode("5555-000-000");
+        guest.setEmail("daza@gmail.com");
 
-        System.out.println(guest2.getName() + " " + guest2.getLastName());
+        System.out.println();
+        x.exibirDados(guest);
 
+    }
+
+    public void exibirDados(Guest guest){
+        System.out.println("---- Guest Information ----");
+        System.out.println("Name...." + guest.getName());
+        System.out.println("Lastname...." + guest.getLastName());
+        System.out.println("Address...." + guest.getAddress());
+        System.out.println("ZipCode...." + guest.getZipCode());
+        System.out.println("City...." + guest.getCity());
+        System.out.println("State...." + guest.getState());
+        System.out.println("E-mail...." + guest.getEmail());
     }
 }
